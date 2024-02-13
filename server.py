616836683c9bb1d7ffc3d589d15dd7bc054549eb
Sara Saber <sara.saber.7006@student.lu.se>
2024-02-13 17:07:17 +0100

    thuesday commit

--- server.py
+++ server.py
@@ -133,10 +133,10 @@ def post_movies():
 
 @get('/movies')
 def get_movies():
-    c = db.curson()
+    c = db.cursor()
     c.execute(
           """
-          SELECT imdb_key, title, year
+          SELECT imdb_key, title, prod_year
           FROM movie
           """
     )
@@ -144,10 +144,100 @@ def get_movies():
     if not found:
         response.status = 400
         return "No movies found"
-    response_data = [{"imdbKey": imdbKey,
-                      "title": title,
-                      "year": year} for imdbKey, title, year in found]
-    response.status = 200
+    else:
+        response_data = [{"imdbKey": imdbKey,
+                        "title": title,
+                        "year": year} for imdbKey, title, year in found]
+        response.status = 200
     return {"data": response_data}
+
+
+
+@get('/movies/<imdb_key>')
+def get_movies_with_ids(imdb_key):
+    c = db.cursor()
+    c.execute(
+        """
+        SELECT   imdb_key, title, prod_year
+        FROM     movie
+        WHERE    imdb_key = ?
+        """,
+        [imdb_key]
+    )
+    found = [{"imdbKey": imdbKey,
+              "title": title,
+              "year": year} for imdbKey, title, year in c]
+    response.status = 200
+    if len(found) == 0:
+         response.status = 400
+         return {"data": []}
+    return {"data": found}
+
+@get('/performances')
+def get_performances():
+    c = db.cursor()
+    c.execute(
+          """
+          SELECT screening_id, date, start_time, title, prod_year, theater_name, capacity
+          FROM screening
+          INNER JOIN movie using(imdb_key)
+          INNER JOIN theater ON screening.theater_name == theater.name
+          """
+    )
+    found = c.fetchall()
+    if not found:
+        response.status = 400
+        return "No performances found"
+    else:
+        response_data = [{"performanceId": screeningId,
+                          "date": date,
+                          "startTime": startTime,
+                          "title": title,
+                          "year": year,
+                          "theater": theater, 
+                          "remainingSeats": remaining_seats} for screeningId, date, startTime, title, year, theater, remaining_seats in found]
+        response.status = 200
+    return {"data": response_data}
+
+@post('/tickets')
+def post_tickets():
+    purchase = request.json
+    c = db.cursor()
+    try: 
+        c.execute(
+            """
+            WITH customer(username) AS (
+                SELECT username 
+                FROM customer
+                WHERE username = ? AND password = ?
+            ),
+            seats(screening_id, available_seats) AS (
+                SELECT screening_id, (capacity - count(*)) as available_seats
+                FROM ticket
+                INNER JOIN theater ON ticket.theater_name == theater.name
+                WHERE screening_id = ?
+                GROUP BY screening_id
+                HAVING available_seats > 0
+            )
+            INSERT INTO ticket (username, screening_id) 
+            VALUES (customer.username, seats.screening_id)
+            returning ticket_id
+            """
+            (purchase["username"], hash(purchase["pwd"]), purchase['performanceId'])
+        )
+        found = c.fetchone()
+        if not found:
+            response.status = 400
+            return "oh noo didnt work "
+        else:
+            db.commit()
+            response.status = 201
+            ticket = found
+            return f"/tickets/{ticket[0]}"
+    except sqlite3.IntegrityError:
+            response.status = 409
+            return "No available seats" 
+
+
 
 run(host='localhost', port=PORT)
